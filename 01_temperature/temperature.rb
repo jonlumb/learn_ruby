edcb6cebe29a35d6d5eff4bb5605fca9cae47a14
Jon Lumb <jonlumb@hotmail.com>
2019-04-02 19:42:33 +0200

    00 to 02 completed

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,9 @@
-#write your code here
+def ftoc(temperature)
+    $CONVERSION_FACTOR = (5.0/9.0)
+    (temperature.to_f - 32) * $CONVERSION_FACTOR
+end
+
+def ctof(temperature)
+    $CONVERSION_FACTOR = (9.0/5.0)
+    (temperature.to_f * $CONVERSION_FACTOR) + 32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,41 @@
-#write your code here
+def add(a,b)
+    a + b
+end
+
+def subtract(a,b)
+    a - b
+end
+
+def sum(array)
+    total = 0
+    if array == []
+        return 0
+    else
+        array.each do |x|
+            total += x
+        end
+    end
+
+    return total
+end
+
+def multiply(*array)
+    total = 1
+    if array == []
+        return "I need some numbers"
+    else
+        array.each do |x|
+            total *= x
+        end
+    end
+    return total
+end
+
+def power(base,power)
+    return base**power
+end
+
+
+def factorial(integer)
+    return (1..integer).inject(:*) || 1
+end
