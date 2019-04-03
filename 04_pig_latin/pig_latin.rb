260967f8169a8830440fb57c3548d3b6f13dca5a
Jon Lumb <jonlumb@hotmail.com>
2019-04-03 17:14:07 +0200

    05 - Book Titles completed

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,15 @@
-#write your code here
+vowels = ["a", "e", "i", "o", "u"]
+
+def translate(word)
+    vowels = ["a", "e", "i", "o", "u"]
+    if vowels.include? word.chr
+        return word << "ay"
+    else
+        consonant = word[1..-1] << word[0] << "ay"
+        if vowels.include? consonant.chr
+            return consonant
+        else
+            
+    end
+end
+
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,17 @@
 class Book
-# write your code here
+    attr_reader :title
+
+    def title=new_title
+        words = new_title.split(" ")
+        words = [words[0].capitalize] +
+        words[1..-1].map do |word|
+            short_words = %w{a an and the in of}
+            if short_words.include? word
+                word
+            else
+                word.capitalize
+            end
+        end
+        @title = words.join(" ")
+    end
 end
